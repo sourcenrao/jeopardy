cb0e5b18651f79efb8d1bc45062e03ff5a0ae6b3
Nikhil Rao <sourcenrao@gmail.com>
2021-10-26 18:10:59 -0400

    Import sqlite3 drivers which require gcc for some reason

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+type Clue struct {
+	Round    uint8
+	Value    uint32
+	Double   string
+	Category string
+	Comments string
+	Answer   string
+	Question string
+}
+
+func main() {
+	fmt.Println("Clue")
 }
