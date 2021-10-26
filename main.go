c748e211116da9cfd45fc46a217a41ddc6a5e1ff
Nikhil Rao <sourcenrao@gmail.com>
2021-10-26 19:00:57 -0400

    Load db into array of struct

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Clue struct {
 	Round    uint8
 	Value    uint32
-	Double   string
+	Double   bool
 	Category string
 	Comments string
 	Answer   string
@@ -15,5 +19,39 @@ type Clue struct {
 }
 
 func main() {
-	fmt.Println("Clue")
+	filepath := "./data/clues.db"
+
+	clues, err := LoadData(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(clues[0])
+}
+
+func LoadData(filename string) ([]Clue, error) {
+	db, err := sql.Open("sqlite3", "./data/clues.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM clues")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var clues []Clue
+
+	for rows.Next() {
+		var clue Clue
+		err = rows.Scan(&clue.Round, &clue.Value, &clue.Double, &clue.Category, &clue.Comments, &clue.Answer, &clue.Question)
+		if err != nil {
+			log.Fatal(err)
+		}
+		clues = append(clues, clue)
+	}
+
+	return clues, nil
 }
